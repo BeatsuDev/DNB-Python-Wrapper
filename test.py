6a8b2ec4b84cc8be7fe4a9d4572029a040c8ffb4
BeatsuDev <ericbieszczad@gmail.com>
2021-07-09 08:49:05 +0200

    pipenv + cleanup

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import os
+# API key set in .env if run with pipenv. Alternatively, set it here:
+# import os
+# os.environ["APIKEY"] = "<your-api-key>"
+
+# You can also set whether to use the live api or the sandbox api:
+# os.environ["LIVE"] = "True"
 
 import dnb.currency.conversion
-
-os.environ["APIKEY"] = "test"
-print(os.environ.get("APIKEY", "nothin"))
 
 conversion = dnb.currency.conversion.convert("NOK", "EUR", 50)
 print(conversion)
